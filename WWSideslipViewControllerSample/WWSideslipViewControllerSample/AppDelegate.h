bde8dce027012d58ac427a46e25bf003c2745a1b
oopww1992 <397294213@qq.com>
2014-08-27 15:27:56 +0800

    提交基础程序功能

---
+++ WWSideslipViewControllerSample/WWSideslipViewControllerSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WWSideslipViewControllerSample
+//
+//  Created by 王维 on 14-8-26.
+//  Copyright (c) 2014年 wangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WWSideslipViewControllerSample/WWSideslipViewControllerSample/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  WWSideslipViewControllerSample
+//
+//  Created by 王维 on 14-8-26.
+//  Copyright (c) 2014年 wangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
